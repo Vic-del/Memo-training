9101c27e51584ee2a844b22083a2777e2d44d348
Víctor <victoragullo@outlook.es>
2019-10-14 10:39:29 +0200

    Initial Commit

---
+++ Memo-training/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memo-training
+//
+//  Created by alumnos on 14/10/2019.
+//  Copyright © 2019 Eztena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
