83d70d48c12b5b078b214c9b01026e83933db1ef
Víctor <victoragullo@outlook.es>
2019-10-14 11:10:00 +0200

    no puto funciona esta mierda.

--- Memo-training/ViewController.swift
+++ Memo-training/ViewController.swift
@@ -1,20 +1,58 @@
-//
-//  ViewController.swift
-//  Memo-training
-//
-//  Created by alumnos on 14/10/2019.
-//  Copyright © 2019 Eztena. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class DataViewController: UIViewController {
+    
+    @IBOutlet weak var blue: UIButton!
+    @IBOutlet weak var red: UIButton!
+    @IBOutlet weak var green: UIButton!
+    @IBOutlet weak var black: UIButton!
+    
+    var buttonsArray:[UIButton] = []
+    var pattern:[UIButton] = []
+    var difficulty:Int8 = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buttonsArray.append(blue)
+        buttonsArray.append(red)
+        buttonsArray.append(green)
+        buttonsArray.append(black)
+
+        let pattern = Gamble(buttons: buttonsArray)
+        
+        Lose(pattern: pattern)
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    func Gamble(buttons: [UIButton]) -> [UIButton] {
+        for _ in 0...difficulty {
+            let randomButton = buttonsArray.randomElement()
+            
+            SwitchButton(randomButton: randomButton!)
+            
+            pattern.append(randomButton!)
+            
+            SwitchButton(randomButton: randomButton!)
+        }
+        return pattern
+    }
+    
+    func SwitchButton(randomButton: UIButton) {
+        if randomButton.isHighlighted {
+            randomButton.isHighlighted = false
+        } else{
+            randomButton.isHighlighted = true
+        }
+    }
+    
+    func Lose(pattern: [UIButton]) {
+        /*if pattern {
+         print("score", difficulty)
+         }*/
+    }
 }
-
