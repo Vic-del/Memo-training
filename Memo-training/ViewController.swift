112640a8ac7964068d560e47d5a8b149a3e41b91
Pedro <pedro_soto_aaps1ta1819@cev.com>
2019-10-18 13:06:32 +0200

    ACABADOOOOOOOOOOOOO

--- Memo-training/ViewController.swift
+++ Memo-training/ViewController.swift
@@ -15,16 +15,17 @@ class DataViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startGame()
+    }
+    
+    func startGame() {
         buttonsArray.append(blue)
         buttonsArray.append(red)
         buttonsArray.append(green)
         buttonsArray.append(black)
-        repeat{
-            let pattern = shuffle(buttons: buttonsArray)
-            
-            gamble(pattern: pattern)
-        }while lose(pattern: pattern) == false
+        
+        let pattern = shuffle(buttons: buttonsArray)
+        gamble(pattern: pattern)
     }
     
     func shuffle(buttons: [UIButton]) -> [UIButton] {
@@ -37,7 +38,7 @@ class DataViewController: UIViewController {
     
     func gamble(pattern: [UIButton]) {
         for button in pattern {
-            delay += 1.0
+            delay += 0.5
             DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
                 self.switchButton(randomButton: button)
             }
@@ -49,33 +50,36 @@ class DataViewController: UIViewController {
         randomButton.isHighlighted = false
     }
     
-    func lose(pattern: [UIButton]) -> Bool {
-        if userTry.count == pattern.count {
+    func lose(pattern: [UIButton]){
+        if userTry.count == pattern.count{
             if userTry != pattern{
-                print("you lose!")
-                return false
+            print("you lose!")
             } else{
-                difficulty += 1
-                print(difficulty)
-                return true
+                difficulty + 1
+                print(difficulty, pattern, userTry)
+                self.userTry = []
+                startGame()
             }
         }
-        return true
     }
     
     @IBAction func blue(_ sender: UIButton) {
         userTry.append(blue)
+        lose(pattern: pattern)
     }
     
     @IBAction func red(_ sender: UIButton) {
         userTry.append(red)
+        lose(pattern: pattern)
     }
     
     @IBAction func green(_ sender: UIButton) {
         userTry.append(green)
+        lose(pattern: pattern)
     }
     
     @IBAction func black(_ sender: UIButton) {
         userTry.append(black)
+        lose(pattern: pattern)
     }
 }
