461c1a9a87fcd10098dc6ca88efa2a6626f827cd
Víctor <38455769+Vic-del@users.noreply.github.com>
2019-10-20 21:14:42 +0200

    El front end está más o menos, queda hacer que se adapte de diferentes maneras a los formatos y pasar información en el cambio de pantallas

--- Memo-training/GameOverController.swift
+++ Memo-training/GameOverController.swift
@@ -1,14 +1,15 @@
 //
-//  GameOverController.swift
+//  EndController.swift
 //  Memo-training
 //
-//  Created by Víctor Agulló on 18/10/19.
+//  Created by Víctor Agulló on 20/10/19.
 //  Copyright © 2019 Eztena. All rights reserved.
 //
-
 
 import UIKit
 
 class GameOverController: UIViewController {
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
